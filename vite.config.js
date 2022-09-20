8de381bec3a96ec2d69ad554e3be132e531357de
steadman1 <64872573+steadman1@users.noreply.github.com>
2022-09-20 00:40:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite"
-import Three from "three";
-import Tween from "@tweenjs/tween.js";
-import Stats from "stats.js"
 
 export default defineConfig({
-    base: "/portfolio",
-    plugins: [Three(), Tween(), Stats()]
+    build: {
+        chunkSizeWarningLimit: 600
+    }
 })
