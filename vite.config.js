592531e4c9564b274db4f489adad49be9442e464
steadman1 <64872573+steadman1@users.noreply.github.com>
2023-01-06 10:47:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from "vite"
 
+const root = resolve(__dirname, './')
+const outDir = resolve(__dirname, 'dist')
+
 export default defineConfig({
+    assetsInclude: ['**/*.gltf'],
     build: {
-        chunkSizeWarningLimit: 600
-    },
-    assetsInclude: ['**/*.gltf']
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about', 'index.html'),
+      }
+    }
+    chunkSizeWarningLimit: 600
+  }
 })
